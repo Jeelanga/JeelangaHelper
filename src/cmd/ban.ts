50fb58f17ab15233b367144147d1e1a935b49059
LWJerri <CLWJerri@yandex.ua>
2020-12-28 22:43:59 +0200

    Add ban command

---
+++ src/cmd/ban.ts
@@ -0,0 +1,26 @@
+import { Message } from "discord.js";
+import { lang } from "../app";
+
+export async function banMember(message: Message){
+    await message.delete().catch(err => console.error(err));
+    
+    const messageArray = message.content.split(" ");
+    const args = messageArray.slice(1);
+    
+    if(!args[0]) return message.channel.send(lang.moderation.noUser)
+        .then(msg => msg.delete({ "timeout": 60000 }))
+        .catch(err => console.error(err));
+
+    const user = message.mentions.users.first();
+    const reason = args[1] ? args[1] : lang.moderation.noReason;
+    const member = message.guild.member(user);
+
+    if(member.hasPermission("ADMINISTRATOR")) return message.channel.send(lang.moderation.staff)
+        .then(msg => msg.delete({ "timeout": 60000 }))
+        .catch(err => console.error(err));
+
+    await member.send(`${lang.moderation.banned} **${reason}**`)
+    .then(async () => {
+        await member.ban({ "reason": reason });
+    }).catch(err => console.error(err));
+}
