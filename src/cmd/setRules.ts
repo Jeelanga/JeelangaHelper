adb861f257420c9e5eee482f56b1d5424cef76fc
LWJerri <CLWJerri@yandex.ua>
2020-12-28 22:43:36 +0200

    Add set-rules command

---
+++ src/cmd/setRules.ts
@@ -0,0 +1,14 @@
+import { Message, TextChannel } from "discord.js";
+import messages from "../../messages.json";
+import { bot } from "../app";
+
+export async function setRules(message: Message){
+    await message.delete().catch(err => console.error(err));
+    
+    const channel = bot.channels.cache.get(process.env.RulesChannel) as TextChannel;
+    await channel.send(messages.description).catch(err => console.error(err));
+
+    messages.rules.forEach(async list => {
+        await channel.send(list).catch(err => console.error(err));
+    });
+}
