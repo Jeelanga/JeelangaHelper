cb5e3d32b20df0b3428515964db57d2883c07489
LWJerri <CLWJerri@yandex.ua>
2020-12-28 22:44:07 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { memberAdd } from "./events/userJoin";
 import { memberRemove } from "./events/userLeave";
 import { memberReact } from "./events/reaction";
 
-export const lang = require(`../localization/${process.env.language}.json`);
+export const lang = require(`../localization/${process.env.language ? process.env.language : "ru"}.json`);
 export const bot = new Client({
     "disableMentions": "none",
     "partials": [
@@ -20,10 +20,15 @@ export const bot = new Client({
     ]
 });
 
+bot.login(process.env.token);
+
 bot.on("ready", readyEvent);
 bot.on("guildMemberAdd", memberAdd);
 bot.on("guildMemberRemove", memberRemove);
 bot.on("message", messageEvent);
 bot.on("messageReactionAdd", memberReact);
+bot.on("error", err => console.error(err));
+bot.on("warn", warn => console.warn(warn));
 
-bot.login(process.env.token);
+process.on("unhandledRejection", (err) => console.error(err));
+process.on("uncaughtException", (err) => console.error(err));
