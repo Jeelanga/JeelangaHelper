9d291989a636fc3db7c998c23170240fba55d9a5
LWJerri <CLWJerri@yandex.ua>
2020-12-28 22:43:18 +0200

    Add set-welcome command

--- src/cmd/setWelcome.ts
+++ src/cmd/setWelcome.ts
@@ -1,10 +1,10 @@
 import { Message, TextChannel } from "discord.js";
 import { bot } from "../app";
-import text from "../../messages.json";
+import messages from "../../messages.json";
 
 export async function setWelcome(message: Message){
     await message.delete().catch(err => console.error(err));
     
     const channel = bot.channels.cache.get(process.env.WelcomeChannel) as TextChannel;
-    await channel.send(text.welcome).catch(err => console.error(err));
+    await channel.send(messages.welcome).catch(err => console.error(err));
 }
