3b584c9cbd6926f4eeb54cf2f375c408e32bc9e3
LWJerri <CLWJerri@yandex.ua>
2020-12-28 15:23:50 +0200

    Add help command

---
+++ src/cmd/help.ts
@@ -0,0 +1,30 @@
+import { Message, MessageEmbed } from "discord.js";
+import { bot, lang } from "../app";
+import time from "humanize-duration";
+
+export async function helpCMD(message: Message){
+    await message.delete();
+
+    const guild = bot.guilds.cache.get(message.guild.id);
+
+    const HelpEmbed = new MessageEmbed()
+        .setTimestamp()
+        .setThumbnail(guild.iconURL({ "size": 4096, "dynamic": true }))
+        .setColor(process.env.EmbedGreen)
+        .setTitle(`${bot.user.username} » ${lang.helpMenu.name}`)
+        .addFields(
+            { name: lang.helpMenu.helpCMD[0], value: lang.helpMenu.helpCMD[1], inline: true },
+            { name: lang.helpMenu.welcomeCMD[0], value: lang.helpMenu.welcomeCMD[1], inline: true },
+            { name: lang.helpMenu.rulesCMD[0], value: lang.helpMenu.rulesCMD[1], inline: true },
+            { name: lang.helpMenu.ticketCMD[0], value: lang.helpMenu.ticketCMD[1], inline: true },
+            { name: lang.helpMenu.kickCMD[0], value: lang.helpMenu.kickCMD[1], inline: true },
+            { name: lang.helpMenu.banCMD[0], value: lang.helpMenu.banCMD[1], inline: true },
+            { name: lang.helpMenu.ping, value: bot.ws.ping, inline: true },
+            { name: lang.helpMenu.users, value: bot.users.cache.size, inline: true },
+            { name: lang.helpMenu.uptime, value: time(bot.uptime, { "language": process.env.language, "round": true }), inline: true },
+        )
+        .setFooter(lang.helpMenu.name, guild.iconURL({ size: 4096, dynamic: true }));
+
+    const msg = await message.channel.send({ "embed": HelpEmbed });
+    await msg.delete({ timeout: 30000 });
+}
