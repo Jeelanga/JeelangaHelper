ac5862208f9130deeddf399a26fbcc2a15843a25
LWJerri <CLWJerri@yandex.ua>
2020-12-28 19:38:02 +0200

    Add .catch()

--- src/cmd/setWelcome.ts
+++ src/cmd/setWelcome.ts
@@ -1,10 +1,10 @@
 import { Message, TextChannel } from "discord.js";
 import { bot } from "../app";
+import text from "../../messages.json";
 
 export async function setWelcome(message: Message){
-    await message.delete();
-    const header = "Добро пожаловать на сервер **Jeelanga**!\n\nНа данный момент бот в разработке, поэтому описание будет предоставлено позже <:waifu_angru:791307126986506310>";
+    await message.delete().catch(err => console.error(err));
     
     const channel = bot.channels.cache.get(process.env.WelcomeChannel) as TextChannel;
-    await channel.send(header);
+    await channel.send(text.welcome).catch(err => console.error(err));
 }
