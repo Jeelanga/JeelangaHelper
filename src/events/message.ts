b0a228bdaf02f8823d2ccdc67b102dd61cdb8f13
LWJerri <CLWJerri@yandex.ua>
2021-01-01 04:14:30 +0200

    Add edit command

--- src/events/message.ts
+++ src/events/message.ts
@@ -10,6 +10,7 @@ import { setTicket } from "../cmd/setTicket";
 import { kickMember } from "../cmd/kick";
 import { banMember } from "../cmd/ban";
 import { lang } from "../app";
+import { editMessage } from "../cmd/edit";
 
 export async function messageEvent(message: Message){
     if(message.author.bot || message.channel.type == "dm") return;
@@ -31,6 +32,7 @@ export async function messageEvent(message: Message){
     if(litter == "set-ticket") return setTicket(message);
     if(litter == "kick") return kickMember(message);
     if(litter == "ban") return banMember(message);
+    if(litter == "edit") return editMessage(message);
     
     if(message.channel.parent.id == process.env.TicketCategory || message.channel.parent.id == "791732950248456213" ||
     message.channel.parent.id == "791732736297009163" || message.member.hasPermission("ADMINISTRATOR")) return;
@@ -40,21 +42,24 @@ export async function messageEvent(message: Message){
     if(spamnya.sameMessages(3, 60000)){
         await message.delete().catch(err => console.error(err));
 
-        const msg = await message.member.send(lang.member.spam);
-        await msg.delete({ "timeout": 60000 });
+        const content = lang.member.deleted.replace("{{ reason }}", lang.moderation.reasons[0])
+        const msg = await message.member.send(content);
+        await msg.delete({ "timeout": 60000 }).catch(err => console.error(err));
     }
 
     if(caps.isCapsed(message.content, { "percentage": 50 })){
         await message.delete().catch(err => console.error(err));
 
-        const msg = await message.member.send(lang.member.caps);
-        await msg.delete({ "timeout": 60000 });
+        const content = lang.member.deleted.replace("{{ reason }}", lang.moderation.reasons[1])
+        const msg = await message.member.send(content);
+        await msg.delete({ "timeout": 60000 }).catch(err => console.error(err));
     }
 
     if(link.includes(message.content, { "withSpaces": true })){
         await message.delete().catch(err => console.error(err));
 
-        const msg = await message.member.send(lang.member.link);
-        await msg.delete({ "timeout": 60000 });
+        const content = lang.member.deleted.replace("{{ reason }}", lang.moderation.reasons[2])
+        const msg = await message.member.send(content);
+        await msg.delete({ "timeout": 60000 }).catch(err => console.error(err));
     }
 }
