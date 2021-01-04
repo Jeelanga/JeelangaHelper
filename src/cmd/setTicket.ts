354feede030293b4ae6a297a0ed76de3164cdb01
LWJerri <CLWJerri@yandex.ua>
2021-01-04 16:43:12 +0200

    Update set-welcome/rules/ticket files

--- src/cmd/setTicket.ts
+++ src/cmd/setTicket.ts
@@ -1,20 +1,28 @@
 import { Message, MessageEmbed, TextChannel } from "discord.js";
 import { bot, lang } from "../app";
+import { envConf } from "../settings";
 
-export async function setTicket(message: Message){
-    await message.delete().catch(err => console.error(err));
-    if(message.author.id != process.env.OwnerID) return;
+export async function setTicket(message: Message) {
+    await message.delete().catch((err: any) => console.error(err));
+    if (message.author.id != envConf.OwnerID) return;
 
     const guild = bot.guilds.cache.get(message.guild.id);
 
     const TicketSetup = new MessageEmbed()
         .setTimestamp()
-        .setThumbnail(guild.iconURL({ "size": 4096, "dynamic": true }))
-        .setColor(process.env.EmbedGreen)
+        .setThumbnail(guild.iconURL({ size: 4096, dynamic: true }))
+        .setColor(envConf.EmbedGreen)
         .setTitle(`${bot.user.username} » ${lang.ticket.title}`)
         .setDescription(lang.ticket.create)
-        .setFooter(lang.helpMenu.name, guild.iconURL({ size: 4096, dynamic: true }));
+        .setFooter(
+            lang.helpMenu.name,
+            guild.iconURL({ size: 4096, dynamic: true })
+        );
 
-    const channel = bot.channels.cache.get(process.env.SupportChannel) as TextChannel;
-    await (await channel.send({ "embed": TicketSetup })).react(process.env.CreateReaction).catch(err => console.error(err));
+    const channel = bot.channels.cache.get(
+        envConf.SupportChannel
+    ) as TextChannel;
+    await (await channel.send({ embed: TicketSetup }))
+        .react(envConf.CreateReaction)
+        .catch((err: any) => console.error(err));
 }
