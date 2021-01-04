a672cba5f7e912b98e58d24511c7cbb2c18832b3
LWJerri <CLWJerri@yandex.ua>
2021-01-04 16:43:48 +0200

    Updaye help/edit commands

--- src/cmd/edit.ts
+++ src/cmd/edit.ts
@@ -1,8 +1,9 @@
 import { Message } from "discord.js";
+import { envConf } from "../settings";
 
-export async function editMessage(message: Message){
-    await message.delete().catch(err => console.error(err));
-    if(message.author.id != process.env.OwnerID) return;
+export async function editMessage(message: Message) {
+    await message.delete().catch((err: any) => console.error(err));
+    if (message.author.id != envConf.OwnerID) return;
 
     const messageArray = message.content.split(" ");
     const args = messageArray.slice(1);
@@ -10,8 +11,8 @@ export async function editMessage(message: Message){
     const messageID = args[0];
     const messageContent = args.slice(1).join(" ");
 
-    if(!messageID) return;
+    if (!messageID) return;
 
     const content = await message.channel.messages.fetch(messageID);
-    await content.edit(messageContent).catch(err => console.error(err));
+    await content.edit(messageContent).catch((err: any) => console.error(err));
 }
--- src/cmd/help.ts
+++ src/cmd/help.ts
@@ -1,32 +1,77 @@
 import { Message, MessageEmbed } from "discord.js";
 import { bot, lang } from "../app";
 import time from "humanize-duration";
+import { envConf } from "../settings";
 
-export async function helpCMD(message: Message){
-    await message.delete().catch(err => console.error(err));
-    if(message.author.id != process.env.OwnerID) return;
+export async function helpCMD(message: Message) {
+    await message.delete().catch((err: any) => console.error(err));
+    if (message.author.id != envConf.OwnerID) return;
 
     const guild = bot.guilds.cache.get(message.guild.id);
 
     const HelpEmbed = new MessageEmbed()
         .setTimestamp()
-        .setThumbnail(guild.iconURL({ "size": 4096, "dynamic": true }))
-        .setColor(process.env.EmbedGreen)
+        .setThumbnail(guild.iconURL({ size: 4096, dynamic: true }))
+        .setColor(envConf.EmbedGreen)
         .setTitle(`${bot.user.username} » ${lang.helpMenu.name}`)
         .addFields(
-            { name: lang.helpMenu.helpCMD[0], value: lang.helpMenu.helpCMD[1], inline: true },
-            { name: lang.helpMenu.welcomeCMD[0], value: lang.helpMenu.welcomeCMD[1], inline: true },
-            { name: lang.helpMenu.rulesCMD[0], value: lang.helpMenu.rulesCMD[1], inline: true },
-            { name: lang.helpMenu.ticketCMD[0], value: lang.helpMenu.ticketCMD[1], inline: true },
-            { name: lang.helpMenu.kickCMD[0], value: lang.helpMenu.kickCMD[1], inline: true },
-            { name: lang.helpMenu.banCMD[0], value: lang.helpMenu.banCMD[1], inline: true },
-            { name: lang.helpMenu.editCMD[0], value: lang.helpMenu.editCMD[1], inline: true },
+            {
+                name: lang.helpMenu.helpCMD[0],
+                value: lang.helpMenu.helpCMD[1],
+                inline: true,
+            },
+            {
+                name: lang.helpMenu.welcomeCMD[0],
+                value: lang.helpMenu.welcomeCMD[1],
+                inline: true,
+            },
+            {
+                name: lang.helpMenu.rulesCMD[0],
+                value: lang.helpMenu.rulesCMD[1],
+                inline: true,
+            },
+            {
+                name: lang.helpMenu.ticketCMD[0],
+                value: lang.helpMenu.ticketCMD[1],
+                inline: true,
+            },
+            {
+                name: lang.helpMenu.kickCMD[0],
+                value: lang.helpMenu.kickCMD[1],
+                inline: true,
+            },
+            {
+                name: lang.helpMenu.banCMD[0],
+                value: lang.helpMenu.banCMD[1],
+                inline: true,
+            },
+            {
+                name: lang.helpMenu.editCMD[0],
+                value: lang.helpMenu.editCMD[1],
+                inline: true,
+            },
             { name: lang.helpMenu.ping, value: bot.ws.ping, inline: true },
-            { name: lang.helpMenu.users, value: bot.users.cache.size, inline: true },
-            { name: lang.helpMenu.uptime, value: time(bot.uptime, { "language": process.env.language, "round": true }), inline: true },
+            {
+                name: lang.helpMenu.users,
+                value: bot.users.cache.size,
+                inline: true,
+            },
+            {
+                name: lang.helpMenu.uptime,
+                value: time(bot.uptime, {
+                    language: envConf.language,
+                    round: true,
+                }),
+                inline: true,
+            }
         )
-        .setFooter(lang.helpMenu.name, guild.iconURL({ size: 4096, dynamic: true }));
+        .setFooter(
+            lang.helpMenu.name,
+            guild.iconURL({ size: 4096, dynamic: true })
+        );
 
-    const output = await message.channel.send({ "embed": HelpEmbed });
-    await output.delete({ "timeout": 60000 }).catch(err => console.error(err));
+    const output = await message.channel.send({ embed: HelpEmbed });
+    await output
+        .delete({ timeout: 60000 })
+        .catch((err: any) => console.error(err));
 }
