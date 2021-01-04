e0ed7eb1b21ce53e4034da6e8aafcab7bf4acf07
LWJerri <CLWJerri@yandex.ua>
2021-01-04 16:42:47 +0200

    Update member join/leave files

--- src/events/userJoin.ts
+++ src/events/userJoin.ts
@@ -1,18 +1,24 @@
-require("dotenv").config();
-
 import { GuildMember, MessageEmbed, TextChannel } from "discord.js";
 import { bot, lang } from "../app";
+import { envConf } from "../settings";
 
-export async function memberAdd(member: GuildMember){
+export async function memberAdd(member: GuildMember) {
     const guild = bot.guilds.cache.get(member.guild.id);
 
     const NewMember = new MessageEmbed()
-        .setColor(process.env.EmbedGreen)
+        .setColor(envConf.EmbedGreen)
         .setTimestamp()
-        .setThumbnail(member.user.displayAvatarURL({ size: 4096, dynamic: true }))
-        .setTitle(`${bot.user.username} » ${member.user.username} (${member.user.id})`)
-        .setFooter(lang.member.join, guild.iconURL({ size: 4096, dynamic: true }));
+        .setThumbnail(
+            member.user.displayAvatarURL({ size: 4096, dynamic: true })
+        )
+        .setTitle(
+            `${bot.user.username} » ${member.user.username} (${member.user.id})`
+        )
+        .setFooter(
+            lang.member.join,
+            guild.iconURL({ size: 4096, dynamic: true })
+        );
 
-    const channel = bot.channels.cache.get(process.env.AdminChannel) as TextChannel;
-    await channel.send(NewMember).catch(err => console.error(err));
+    const channel = bot.channels.cache.get(envConf.AdminChannel) as TextChannel;
+    await channel.send(NewMember).catch((err: any) => console.error(err));
 }
